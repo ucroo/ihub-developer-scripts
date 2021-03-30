4db93934b79a3ca9efa1d30eef2d5a9e3de9f5f8
David Hagan <dave@ucroo.com>
2021-03-30 12:09:48 +1100

    somehow I missed this file?

--- uploadResourceCollection.sh
+++ uploadResourceCollection.sh
@@ -17,15 +17,19 @@ esac
 
 source setEnvForUpload.sh $ENVIRONMENT
 
-if [ -z $COOKIE ]
-then
-  echo "no valid cookie"
-  return 1
-fi
-
 rm ${FLOW}.zip
 zip -r ${FLOW}.zip $FLOW
 
-curl $CURL_ARGS -X POST -H "Cookie: JSESSIONID=$COOKIE" -H "Content-Type: application/octet-stream" -H "format: zip" -H "name: ${FLOW}" "$HOST/repository/resourceCollections" --data-binary "@${FLOW}.zip"
-rm ${FLOW}.zip
+if [ -z $FLOW_TOKEN ] ;
+then
+	if [ -z $COOKIE ]
+	then
+		echo "no valid cookie"
+		return 1
+	fi
+	curl $CURL_ARGS -X POST -H "Cookie: JSESSIONID=$COOKIE" -H "Content-Type: application/octet-stream" -H "format: zip" -H "name: ${FLOW}" "$HOST/repository/resourceCollections" --data-binary "@${FLOW}.zip"
+else
+	curl $CURL_ARGS -X POST -H "flow-token: $FLOW_TOKEN" -H "Content-Type: application/octet-stream" -H "format: zip" -H "name: ${FLOW}" "$HOST/repository/resourceCollections" --data-binary "@${FLOW}.zip"
+fi
 
+rm ${FLOW}.zip
