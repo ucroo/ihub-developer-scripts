0c22950335a6b913506e077f635451e5d85f58dd
David Hagan <dave@ucroo.com>
2021-07-22 15:56:10 +1000

    added a function to delete resource collections by API

---
+++ deleteResourceCollection.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+FLOW="$1"
+ENVIRONMENT="$2"
+
+case $# in
+  2)
+    ENVIRONMENT="$2"
+    ;;
+  1)
+    ENVIRONMENT="local"
+    ;;
+  *)
+    echo "not enough arguments supplied.  You must supply the flowName to this command, and have a creds/${ENVIRONMENT}.username and creds/${ENVIRONMENT}.password file populated."
+    return 1
+    ;;
+esac    
+
+source setEnvForUpload.sh $ENVIRONMENT
+
+if [ -z $FLOW_TOKEN ] ;
+then
+	if [ -z $COOKIE ]
+	then
+		echo "no valid cookie"
+		return 1
+	fi
+	curl $CURL_ARGS -X DELETE -H "Cookie: JSESSIONID=$COOKIE" -H "Content-Type: application/json" "$HOST/repository/resourceCollections/$FLOW" 
+else
+	curl $CURL_ARGS -X DELETE -H "flow-token: $FLOW_TOKEN" -H "Content-Type: application/json" "$HOST/repository/resourceCollections/$FLOW" 
+fi
+
