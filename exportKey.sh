36cde928afc1a50910d9c0aa31356c6e2a57a849
David Hagan <dave@ucroo.com>
2021-09-03 17:12:03 +1000

    added scripts around the encryptors

---
+++ exportKey.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+FLOW="$1"
+ENVIRONMENT="$2"
+
+case $# in
+  2)
+    ENVIRONMENT="$2"
+    ;;
+  1)
+    ENVIRONMENT="local"
+    ;;
+  *)
+    echo "not enough arguments supplied.  You must supply the flowName to this command."
+    return 1
+    ;;
+esac    
+
+source setEnvForUpload.sh $ENVIRONMENT
+
+if [ -z $FLOW_TOKEN ] ;
+then
+	return 1
+else
+	curl $CURL_ARGS -X GET -H "flow-token: $FLOW_TOKEN" "$HOST/auth/s2s/encryption/exportKey" 
+fi
+
+
---
+++ importKey.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+FLOW="$1"
+ENVIRONMENT="$2"
+
+case $# in
+  2)
+    ENVIRONMENT="$2"
+    ;;
+  1)
+    ENVIRONMENT="local"
+    ;;
+  *)
+    echo "not enough arguments supplied.  You must supply the flowName to this command."
+    return 1
+    ;;
+esac    
+
+source setEnvForUpload.sh $ENVIRONMENT
+
+if [ -z $FLOW_TOKEN ] ;
+then
+	return 1
+else
+	curl $CURL_ARGS -X POST -H "flow-token: $FLOW_TOKEN" -H "Content-Type: application/json" "$HOST/auth/s2s/encryption/importKey" --data-binary "@$FLOW"
+fi
+
