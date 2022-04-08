22858d06e9849d8e9ab4ae7605c02633f5d69c91
Tim Stewart <90655759+timjstewart1024@users.noreply.github.com>
2022-04-08 11:00:42 -0600

    Added script to check console filters.

---
+++ checkConsoleFilters.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+# checkConsoleFilters -- examines a file downloaded from <BASE_URL>/repository/flows
+# to see if its consoleFilters are configured correctly.
+#
+# Note: this script is inefficient due to its repeated parsing of the JSON file but
+#       it is fast enough to be usable.
+
+set -euo pipefail
+
+die() {
+    echo "$*"
+    exit 1
+}
+
+[ "$#" = "1" ]            || die "usage: $(basename "$0") FILE"
+[ -r "$1" ]               || die "error: $1 is not readable"
+command -v jq > /dev/null || die "error: jq must be installed"
+file=$1
+
+indent()          { sed 's/^/  /'; }
+valueNotEqualTo() { compareValue "$1" "!=" "$2"; }
+valueEqualTo()    { compareValue "$1" "==" "$2"; }
+
+compareValue() {
+    property=$1
+    operator=$2
+    value=$3
+    output=$(jq -r ".[] | {
+        name,
+        ${property}: .consoleFilter.${property}
+    } | select (
+        .${property} ${operator} ${value}
+    ) | select(.name | ascii_downcase | contains(\"test\") | not) | .name" "$file")
+    if [ -n "${output}" ] ; then
+        echo "flows where $property $operator $value:"
+        echo "${output}" | indent
+    fi
+}
+
+main() {
+    valueNotEqualTo error true
+    valueEqualTo synchronous true
+    valueEqualTo debug true
+    valueEqualTo info true
+    valueEqualTo warn true
+    valueEqualTo other true
+    valueEqualTo forceStreams true
+    valueEqualTo sendToCentralReporter true
+    valueEqualTo summarizeToCentral true
+    valueEqualTo saveStateToDb true
+    valueEqualTo consoleMessageSynchronous true
+    valueEqualTo logPayload true
+}
+
+main
