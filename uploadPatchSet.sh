a8fced4d137cf3b31df4fef07c562acf7816e568
Tim Stewart <90655759+timjstewart1024@users.noreply.github.com>
2022-06-14 15:54:39 -0600

    accidentally deleted the file uploadPatchSet.sh

---
+++ uploadPatchSet.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+FLOW="$1"
+ENVIRONMENT="$2"
+
+case $# in
+  2)
+    ENVIRONMENT="$2"
+    ;;
+  1)
+    ENVIRONMENT="local"
+    ;;
+  *)
+    echo "not enough arguments supplied.  You must supply the flowName to this command."
+    return 1
+    ;;
+esac    
+
+source setEnvForUpload.sh $ENVIRONMENT
+
+if [ -z $FLOW_TOKEN ] ;
+then
+	return 1
+else
+	http_response=$(curl $CURL_ARGS -s -o uploadFlowResponse.txt -w "%{http_code}" -X POST -H "flow-token: $FLOW_TOKEN" -H "Content-Type: application/json" "$HOST/repository/patchSets" --data-binary "@src/main/patchSets/$FLOW.json")
+fi
+
+if [ $http_response != "200" ];
+then
+  if [ $http_response == "302" ];
+  then
+    echo "Got unexpected HTTP response ${http_response}. This is likely due to your token being incorrect."
+  else
+    echo "Got unexpected HTTP response ${http_response}. This is likely an error."
+  fi
+else
+  cat uploadFlowResponse.txt
+fi
+
+[ -e uploadFlowResponse.txt ] && rm uploadFlowResponse.txt
