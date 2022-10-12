523d72630396a656e5abd6de92a01b9d36310cd9
Tim Stewart <90655759+timjstewart1024@users.noreply.github.com>
2022-10-12 12:04:45 -0600

    Now handles encrypted shared configuration better.

--- downloadFlowEntities.sh
+++ downloadFlowEntities.sh
@@ -2,6 +2,18 @@
 
 set -euo pipefail
 
+die() {
+    >&2 echo -e "error: $1"
+    exit 1
+}
+
+require() {
+    command -v "$1" > /dev/null || die "$1 is not in the \$PATH"
+}
+
+require curl
+require jq
+
 CURL_ARGS="${CURL_ARGS:-}"
 
 case $# in
@@ -19,7 +31,8 @@ esac
 
 source setEnvForUpload.sh $environment
 
-mkdir -p src/main/{flows,flowResources,sharedConfig,triggerers}
+mkdir -p src/main/{flows,flowResources,sharedConfig,triggerers,patchSets}
+
 
 function downloadJsonFile {
     entityType=$1
@@ -34,6 +47,8 @@ function downloadJsonFile {
         outputFile="./src/main/${outputDirectory}/${environment}.json"
         if echo "${output}" | grep -v "Status: "| jq empty >& /dev/null ; then
             echo "${output}" | grep -v "Status: " > "${outputFile}"
+            noMetaData=$(jq 'del(.[].metadata)' "${outputFile}")
+            echo "${noMetaData}" > "${outputFile}"
         else
             >&2 echo "server did not return valid JSON.  Is your token valid?"
             return 1
@@ -55,15 +70,20 @@ function downloadZipFile {
         if [ -r "${outputFile}" ] ; then
             unzip "${outputFile}" -d "./src/main/${outputDirectory}/" > /dev/null || true
             rm "${outputFile}"
+            # Use jq to format the JSON files that were in the zip file.
+            while IFS= read -r -d '' jsonFile ; do
+                cat <<< "$(jq < "${jsonFile}")" > "${jsonFile}"
+            done < <(find "./src/main/${outputDirectory}" -iname '*.json' -print0)
         fi
    fi
 }
 
-#                Flow Route            Directory (if different)
-#                -------------------   --------------------------
-downloadJsonFile flows
-downloadJsonFile sharedConfig
+#                Flow Route            Directory        Query String Parameters
+#                -------------------   ------------     ---------------------------
+downloadJsonFile flows                 flows
+downloadJsonFile sharedConfig          sharedConfig     '?encrypted=true'
 downloadJsonFile flowTriggerers        triggerers
+downloadJsonFile patchSets             patchSets
 downloadZipFile  resourceCollections   flowResources
 
 # Format the JavaScript in the flow JSON file.
