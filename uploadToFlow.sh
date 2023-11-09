cfb15745c90a405a8ff434a4ec9a269c23acc547
Tim Stewart <90655759+timjstewart1024@users.noreply.github.com>
2023-11-09 13:43:21 -0700

    made the script safer to run and refactored the code

--- uploadToFlow.sh
+++ uploadToFlow.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+set -eou pipefail
+
 die() {
     >&2 echo "$*"
     exit 1
@@ -9,12 +11,12 @@ require() {
     command -v "$1" > /dev/null || die "$1 is not in the \$PATH"
 }
 
-if [ "$#" -ne "2" ] ; then
-    die "usage: $(basename "$0") SERVER ENTITY_FILE"
+if [ "$#" -lt "2" ] ; then
+    die "usage: $(basename "$0") SERVER ENTITY_FILE.."
 fi
 
 server=$1
-entityFile=$2
+shift
 
 # Make sure that scripts we call are in the PATH.
 require uploadFlow.sh
@@ -23,29 +25,97 @@ require uploadTrigger.sh
 require uploadSharedConfig.sh
 require uploadRecipe.sh
 
-if [ ! -r "${entityFile}" ] ; then
-    die "can't read file: ${entityFile}"
-fi
+detectType() {
+  fileName="$1"
+  absolutePath=$(readlink -f "$fileName")
 
-if [[ "${entityFile}" =~ flows/ ]] ; then
-    cd "$(dirname "${entityFile}")/../../.." || exit 1
-    uploadFlow.sh "$(basename "${entityFile%%.json}")" "${server}"
+  if [[ "${absolutePath}" =~ flows/ ]] ; then
+    echo "flow"
+  elif [[ "${absolutePath}" =~ flowResources/ ]] ; then
+    echo "resource"
+  elif [[ "${absolutePath}" =~ triggerers/ ]] ; then
+    echo "trigger"
+  elif [[ "${absolutePath}" =~ sharedConfig/ ]] ; then
+    echo "config"
+  elif [[ "$(pwd)/${absolutePath}" =~ recipe ]] ; then
+    echo "recipe"
+  else
+    echo "unknown"
+  fi
+}
 
-elif [[ "${entityFile}" =~ flowResources/ ]] ; then
-    cd "$(dirname "${entityFile}")" || exit 1
-    uploadResourceCollection.sh "$(basename "${entityFile}")" "${server}"
+uploadFlow() {
+  entity="$1"
+  pushd "$(dirname "${entity}")/../../.." > /dev/null || exit 1
+  uploadFlow.sh "$(basename "${entity%%.json}")" "${server}" 
+  popd > /dev/null
+}
 
-elif [[ "${entityFile}" =~ triggerers/ ]] ; then
-    cd "$(dirname "${entityFile}")/../../.." || exit 1
-    uploadTrigger.sh "$(basename "${entityFile%%.json}")" "${server}"
+uploadResource() {
+  entity="$1"
+  pushd "$(dirname "${entity}")" > /dev/null || exit 1
+  uploadResourceCollection.sh "$(basename "${entity}")" "${server}"
+  popd > /dev/null
+}
 
-elif [[ "${entityFile}" =~ sharedConfig/ ]] ; then
-    cd "$(dirname "${entityFile}")/../../.." || exit 1
-    uploadSharedConfig.sh "$(basename "${entityFile%%.json}")" "${server}"
+uploadTrigger() {
+  entity="$1"
+  pushd "$(dirname "${entity}")/../../.." > /dev/null || exit 1
+  uploadTrigger.sh "$(basename "${entity%%.json}")" "${server}"
+  popd > /dev/null
+}
 
-elif [[ "$(pwd)/${entityFile}" =~ recipe ]] ; then
-    cd "$(dirname "${entityFile}")" || exit 1
-    uploadRecipe.sh "$(basename "${entityFile}")" "${server}"
-else
-    die "don't know what to upload."
-fi
+uploadSharedConfig() {
+  entity="$1"
+  pushd "$(dirname "${entity}")/../../.." > /dev/null || exit 1
+  uploadSharedConfig.sh "$(basename "${entity%%.json}")" "${server}"
+  popd > /dev/null
+}
+
+uploadRecipe() {
+  entity="$1"
+  pushd "$(dirname "${entity}")" > /dev/null || exit 1
+  uploadRecipe.sh "$(basename "${entity}")" "${server}"
+  popd > /dev/null
+}
+
+for entity in "$@" ; do
+  echo "Uploading: ${entity} ..."
+
+  if [ ! -r "${entity}" ] ; then
+      >&2 echo "can't read file: ${entity}"
+      continue
+  fi
+
+  type=$(detectType "$entity")
+
+  case "$type" in
+
+    "flow")
+      uploadFlow "${entity}"
+      ;;
+
+    "config")
+      uploadSharedConfig "${entity}"
+      ;;
+
+    "recipe")
+      uploadRecipe "${entity}"
+      ;;
+
+    "trigger")
+      uploadTrigger "${entity}"
+      ;;
+
+    "resource")
+      uploadResource "${entity}"
+      ;;
+
+    *)
+      >&2 echo "unknown file type: ${entity}"
+      ;;
+
+  esac
+  echo
+done
+
