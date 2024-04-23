88ac2eddfc10c4d7b05704ebfb6f8822a2e3a4dd
LSAllenUcroo <louise.allen@ucroo.com>
2024-04-23 14:00:21 +1000

    Now it is possible to only look for recipe executions executed by a particular user.

--- reloadRecipe.sh
+++ reloadRecipe.sh
@@ -1,14 +1,53 @@
 #!/bin/sh
 echo "Run this script from the same directory as your recipe metadata.json"
 echo "Provide an environment shortname which is at least Flow V5.11."
+echo "The first parameter must either be the environment shortname or an integer greater than 0 (the number of days to regard as 'recent' when fetching recent recipeExecutions, defaults to 1 day if not supplied)"
+# Takes in up to three parameters: environment ENV, number of days to fetch recipeExecutions for RECENT, a username to use to filter recipeExecutions RECIPEUSER
+# Accepted combinations:
+#  ENV RECENT RECIPEUSER
+#  ENV RECENT
+#  RECENT RECIPEUSER
+#  RECENT
+#  ENV
+# Where the first parameter supplied is an integer, ENV defaults to 'local'
+# Where no RECENT parameter is supplied, RECENT defaults to 1 day
+RECIPEUSER='none'
 RECENT=0
 case $# in
+  3)
+    ENV=$1
+    if ! [[ $2 =~ ^[0-9]+$ && $2 -gt 0 ]]; then
+      echo "Second parameter must be an integer greater than 0"
+      exit
+    else 
+      RECENT=$2
+    fi
+    RECIPEUSER=$3
+    ;;
   2)
-    ENV="$1"
-    RECENT="$2"
+    if [[ $1 =~ ^[0-9]+$ ]]; then
+      echo "Integer input as first parameter, defaulting to local environment"
+      RECENT=$1
+      RECIPEUSER=$2
+      ENV="local"
+    else
+      ENV="$1"
+      if ! [[ $2 =~ ^[0-9]+$ && $2 -gt 0 ]]; then
+        echo "Days parameter, if provided, must be an integer greater than 0"
+        exit
+      else
+        RECENT=$2
+      fi
+    fi
     ;;
   1)
-    ENV="$1"
+    if [[ $1 =~ ^[0-9]+$ && $1 -gt 0 ]]; then
+      echo "Integer input as first parameter, defaulting to local environment"
+      RECENT=$1
+      ENV="local"
+    else
+      ENV="$1"
+    fi
     ;;
   0)
     ENV="local"
@@ -18,19 +57,8 @@ case $# in
     ;;
 esac
 
-if ! [[ $RECENT =~ ^[0-9]+$ ]]; then
- echo "Second parameter, if provided, must be an integer greater than 0"
- exit
-fi
-
-if [[ $ENV =~ ^[0-9]+$ ]]; then
-  echo "Integer input as first parameter, defaulting to local environment"
-  RECENT=$ENV
-  ENV="local"
-fi
-
 if ! [[ $RECENT -gt 0 ]]; then
-  echo "No valid input for RECENT, defaulting to 1 day"
+  echo "No input specified for RECENT, defaulting to 1 day"
   RECENT=1
 fi
 
@@ -46,9 +74,19 @@ DAY=$(expr 1000 \* 60 \* 60 \* 24 \* $RECENT)
 YESTERDAY=$(expr $NOW - $DAY)
 RECENT_RECIPE_EXECUTIONS=$(curl $CURL_ARGS -s -H "flow-token: $FLOW_TOKEN" "$HOST/repository/auditLogs?type=recipeExecution&start=$YESTERDAY&end=$NOW" )
 HOURS=$(expr 24 \* $RECENT)
-PREVIOUS_ANSWERS=$(jq -r '[.[] | select(.audited.id=="'$RECIPE'")][0] | .audited.input' <<< "$RECENT_RECIPE_EXECUTIONS")
+
+if ! [ $RECIPEUSER = 'none' ]; then
+  PREVIOUS_ANSWERS=$(jq -r '[.[] | select(.audited.id=="'$RECIPE'" and .userName=="'$RECIPEUSER'" )][0] | .audited.input' <<< "$RECENT_RECIPE_EXECUTIONS")
+else
+  PREVIOUS_ANSWERS=$(jq -r '[.[] | select(.audited.id=="'$RECIPE'")][0] | .audited.input' <<< "$RECENT_RECIPE_EXECUTIONS")
+fi
+
 if [ $PREVIOUS_ANSWERS = 'null' ]; then
-  echo "No recipe executions found on this server for" $RECIPE "in last" $HOURS "hours. Rerun through Recipe History."
+  if ! [ $RECIPEUSER = 'none' ]; then
+    echo "No recipe executions by user:" $RECIPEUSER "found on server:" $ENV "for" $RECIPE "in last" $HOURS "hours. Rerun through Recipe History."
+  else
+    echo "No recipe executions found on server:" $ENV "for" $RECIPE "in last" $HOURS "hours. Rerun through Recipe History."
+  fi
 else
   echo "Rerunning previous answers.  If they're incomplete, this will error and you need to go answer them in the interface and rerun this."
   curl $CURL_ARGS -H "flow-token: $FLOW_TOKEN" -H "Content-Type: application/json" "$HOST/repository/recipes/$RECIPE/execute?forceInstallAll=true" --data-binary "$PREVIOUS_ANSWERS"
