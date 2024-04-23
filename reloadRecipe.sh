452a5581c4a0ef4c3d1401f0c5628e2ff2b488ff
LSAllenUcroo <louise.allen@ucroo.com>
2024-04-23 10:53:32 +1000

    Fixing index bug, now not restricted to the last 24 hours, now possible to set the time period through an optional parameter.

--- reloadRecipe.sh
+++ reloadRecipe.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 echo "Run this script from the same directory as your recipe metadata.json"
 echo "Provide an environment shortname which is at least Flow V5.11."
+RECENT=0
 case $# in
+  2)
+    ENV="$1"
+    RECENT="$2"
+    ;;
   1)
     ENV="$1"
     ;;
@@ -12,6 +17,23 @@ case $# in
     return 1
     ;;
 esac
+
+if ! [[ $RECENT =~ ^[0-9]+$ ]]; then
+ echo "Second parameter, if provided, must be an integer greater than 0"
+ exit
+fi
+
+if [[ $ENV =~ ^[0-9]+$ ]]; then
+  echo "Integer input as first parameter, defaulting to local environment"
+  RECENT=$ENV
+  ENV="local"
+fi
+
+if ! [[ $RECENT -gt 0 ]]; then
+  echo "No valid input for RECENT, defaulting to 1 day"
+  RECENT=1
+fi
+
 RECIPE_FAMILY=$(basename "`pwd`")
 RECIPE=$(jq -r ".id" metadata.json)
 echo "Refreshing ${RECIPE_FAMILY} - ${RECIPE}"
@@ -20,21 +42,24 @@ pushd ..
 popd
 source setEnvForUpload.sh "$ENV"
 NOW=$(date +%s000)
-DAY=$(expr 1000 \* 60 \* 60 \* 24)
+DAY=$(expr 1000 \* 60 \* 60 \* 24 \* $RECENT)
 YESTERDAY=$(expr $NOW - $DAY)
 RECENT_RECIPE_EXECUTIONS=$(curl $CURL_ARGS -s -H "flow-token: $FLOW_TOKEN" "$HOST/repository/auditLogs?type=recipeExecution&start=$YESTERDAY&end=$NOW" )
 INDEX=0
 LENGTH=$(jq -r ". | length" <<< "$RECENT_RECIPE_EXECUTIONS")
 for i in $(jq -r ".[] | .audited.id" <<< "$RECENT_RECIPE_EXECUTIONS"); do
- INDEX=$(expr $INDEX + 1)
  if [ "$i" = "$RECIPE" ]; then
     echo "Recent execution of " $i "found at index" $INDEX
     break
  fi
+ INDEX=$(expr $INDEX + 1)
 done
 
+HOURS=$(expr 24 \* $RECENT)
+echo "index" $INDEX
+echo "length" $LENGTH
 if [ $INDEX -ge $LENGTH ]; then
-  echo "No recipe executions found on this server for" $RECIPE "in last 24 hours. Rerun through Recipe History."
+  echo "No recipe executions found on this server for" $RECIPE "in last" $HOURS "hours. Rerun through Recipe History."
 else
   echo "Rerunning previous answers.  If they're incomplete, this will error and you need to go answer them in the interface and rerun this."
   PREVIOUS_ANSWERS=$(jq -r ".[$INDEX] | .audited.input" <<< "$RECENT_RECIPE_EXECUTIONS")
