5cff5a089ddb431168530c573bd56177d6f0de06
LSAllenUcroo <louise.allen@ucroo.com>
2024-04-24 10:48:30 +1000

    Responding to PR comments, adding additional safety and error handling.

--- reloadRecipe.sh
+++ reloadRecipe.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-echo "Run this script from the same directory as your recipe metadata.json"
-echo "Provide an environment shortname which is at least Flow V5.11."
-echo "The first parameter must either be the environment shortname or an integer greater than 0 (the number of days to regard as 'recent' when fetching recent recipeExecutions, defaults to 1 day if not supplied)"
+echo "To provide an environment, use -e followed by an environment shortname. The default environment is local.\nTarget environments must be at least Flow V5.11."
 # Takes in up to three parameters: 
 #   - environment ENV, defaults to 'local'
 #   - number of days to fetch recipeExecutions for RECENT, defaults to 1
@@ -26,26 +24,35 @@ if [ -z "$RECENT" ]; then RECENT=1; fi
 if ! [[ $RECENT =~ ^[0-9]+$ && $RECENT -gt 0 ]]; then echo "Days parameter, if provided, must be an integer greater than 0"; exit; fi
 
 RECIPE_FAMILY=$(basename "`pwd`")
+if [ ! -r "./metadata.json" ] ; then
+   echo "\nError: no metadata.json found.\nRun this script from the same directory as your recipe metadata.json"
+   exit 1
+fi
 RECIPE=$(jq -r ".id" metadata.json)
 echo "Refreshing ${RECIPE_FAMILY} - ${RECIPE}"
-pushd ..
+
+source setEnvForUpload.sh "$ENV" || exit $?
+pushd .. > /dev/null
  uploadRecipe.sh "$RECIPE_FAMILY" "$ENV"
-popd
-source setEnvForUpload.sh "$ENV"
+popd > /dev/null
+
 NOW=$(date +%s000)
 DAY=$(expr 1000 \* 60 \* 60 \* 24 \* $RECENT)
 YESTERDAY=$(expr $NOW - $DAY)
 RECENT_RECIPE_EXECUTIONS=$(curl $CURL_ARGS -s -H "flow-token: $FLOW_TOKEN" "$HOST/repository/auditLogs?type=recipeExecution&start=$YESTERDAY&end=$NOW" )
 HOURS=$(expr 24 \* $RECENT)
 
-if ! [ $RECIPEUSER = 'none' ]; then
+if [ -z "$RECENT_RECIPE_EXECUTIONS" ]; then echo "No recent recipe executions found on this server";exit; fi
+
+if ! [ "$RECIPEUSER" = 'none' ]; then
   PREVIOUS_ANSWERS=$(jq -r '[.[] | select(.audited.id=="'$RECIPE'" and .userName=="'"$RECIPEUSER"'" )][0] | .audited.input' <<< "$RECENT_RECIPE_EXECUTIONS")
 else
   PREVIOUS_ANSWERS=$(jq -r '[.[] | select(.audited.id=="'$RECIPE'")][0] | .audited.input' <<< "$RECENT_RECIPE_EXECUTIONS")
 fi
 
-if [ $PREVIOUS_ANSWERS = 'null' ]; then
-  if ! [ $RECIPEUSER = 'none' ]; then
+
+if [ "$PREVIOUS_ANSWERS" = 'null' ]; then
+  if ! [ "$RECIPEUSER" = 'none' ]; then
     echo "No recipe executions by user:" $RECIPEUSER "found on server:" $ENV "for" $RECIPE "in last" $HOURS "hours. Rerun through Recipe History."
   else
     echo "No recipe executions found on server:" $ENV "for" $RECIPE "in last" $HOURS "hours. Rerun through Recipe History."
